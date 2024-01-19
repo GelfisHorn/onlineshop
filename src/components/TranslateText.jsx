cf21b3010cf08f627d02cabf023c3f3a20bf4fbb
GelfisHorn <130309879+GelfisHorn@users.noreply.github.com>
2024-01-19 15:55:53 -0300

    Fix/Build fix

--- src/components/TranslateText.jsx
+++ src/components/TranslateText.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
 const API_URL = 'https://translation.googleapis.com/language/translate/v2';
 
-export default async function translateText({text, lang}) {
+export default async function TranslateText({text, lang}) {
     const [showingText, setShowingText] = useState(text);
 
     async function handleTranslate() {
